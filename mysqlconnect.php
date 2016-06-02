2f5973e91220227917e63f94b0c3d4b4ffc20ec8
root <ipanamski@gmail.com>
2016-06-02 13:39:59 +0300

    Fix error

--- mysqlconnect.php
+++ mysqlconnect.php
@@ -4,7 +4,7 @@ $HOSTNAME = ""; //Mysql server location ex. "127.0.0.1"
 $DB_USER = ""; //Mysql username ex. "root"
 $DB_PASS = ""; //Mysql password ex. "toor"
 $DATABASE = ""; //Mysql database ex. "EasyAuth"
-$TABLE = "user" //Mysql table for user data ex. "user" !!DON'T MODIFY!!
+$TABLE = "user"; //Mysql table for user data ex. "user" !!DON'T MODIFY!!
 //----------------------------------------------------------------------
 $connection = mysqli_connect($HOSTNAME, $DB_USER, $DB_PASS, $DATABASE);
 if($connection)
