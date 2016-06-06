8e6b6c39f67f5acbd29267288b0e0332c8b4bb32
AMDIvailo <ipanamski@gmail.com>
2016-06-06 23:08:26 +0300

    Added register method example

---
+++ example_reg.php
@@ -0,0 +1,23 @@
+<?php
+//This is basic example on the register method
+require("userMgr.php");
+?>
+<form method='post'>
+<input type='text' name='username'>
+<input type='password' name='password'>
+<input type='submit' name='Register' value='Register'>
+</form>
+<?php
+if(isset($_POST['Register'])) //Check if register button is pressed
+{
+	if($_POST['username'] != "" && $_POST['password'] != "") //Check if fields are not empty
+	{
+		$manager = new userMgr();
+		$manager->register($_POST['username'], $_POST['password'], "test@example.com");
+	}
+	else //If fields are empty
+	{
+		print "You can't create account with empty username or password!";
+	}
+}
+?>
