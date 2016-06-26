a1a3f338ceac86c6896add09799fa2c9bb5a34bb
AMDIvailo <ipanamski@gmail.com>
2016-06-26 21:53:01 +0300

    Add sql file
    sql fill for the easyauth database

---
+++ database.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(100) DEFAULT NULL,
+  `password` varchar(100) DEFAULT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=latin1;
